960fa092731bc487cbbb98864f5188ca5845991d
Michael Mortensen <michael@geekle.io>
2024-09-20 01:27:13 +0200

    :pencil2: fixed typo for DocFX

--- .docfx/PublishDocfxImage.ps1
+++ .docfx/PublishDocfxImage.ps1
@@ -1,3 +1,3 @@
 $version = minver -i -t v -v w
-docker tag yamldotnet-docfx:$version jcr.codebelt.net/geekle/yamldotnet:$version
-docker push yourbranding/classlibrary1:$version
+docker tag yamldotnet-docfx:$version jcr.codebelt.net/geekle/yamldotnet-docfx:$version
+docker push jcr.codebelt.net/geekle/yamldotnet-docfx:$version
